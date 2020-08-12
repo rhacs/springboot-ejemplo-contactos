95903e60d681608ae91db4b64b47aedd28f7ee6d
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-12 13:30:07 -0400

    added initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------------------------
+-- Tabla: contactos
+-- NOTA: Datos generados a través del servicio web de Mockaroo.com
+----------------------------------------------------------------------------------------------------
+
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (1, 'Shamus Eilhersen', '439 Nancy Junction', '7153484624', 'seilhersen0@networksolutions.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (2, 'Horst Zoppie', '2 Declaration Place', '8393738203', 'hzoppie1@cloudflare.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (3, 'Lodovico Mayne', '60462 Hagan Place', '2938046922', 'lmayne2@jiathis.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (4, 'Nertie Steward', '8 Manufacturers Drive', '3457998261', 'nsteward3@ustream.tv');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (5, 'Livvy Bannister', '726 Morning Hill', '7526781346', 'lbannister4@fc2.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (6, 'Corenda Ratnage', '93060 Mifflin Center', '5964446775', 'cratnage5@cloudflare.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (7, 'Raeann Klehyn', '0 Goodland Center', '2838109987', 'rklehyn6@chronoengine.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (8, 'Dorelle Whiffen', '51 Leroy Place', '9808814939', 'dwhiffen7@bloglovin.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (9, 'Giulio Polfer', '6718 Eagle Crest Center', '5684280020', 'gpolfer8@free.fr');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (10, 'Wilbert Nucator', '521 Little Fleur Center', '5765834215', 'wnucator9@squidoo.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (11, 'Murdoch Teligin', '930 Sullivan Point', '5664539942', 'mteligina@cloudflare.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (12, 'Melosa Adnet', '70383 Hazelcrest Junction', '4082212054', 'madnetb@gmpg.org');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (13, 'Brenn Danielot', '04 Tomscot Junction', '1732839088', 'bdanielotc@amazon.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (14, 'Sheba Haynes', '814 Utah Parkway', '3571289732', 'shaynesd@webnode.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (15, 'Robina Presnall', '472 Messerschmidt Trail', '7875818759', 'rpresnalle@opensource.org');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (16, 'Viv Bode', '92150 Ryan Plaza', '1065404494', 'vbodef@telegraph.co.uk');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (17, 'Koo Coupland', '73529 Trailsway Terrace', '9598466950', 'kcouplandg@java.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (18, 'Maximo Humbert', '1 Utah Avenue', '2711755563', 'mhumberth@multiply.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (19, 'Marlo Nanson', '69 Vera Junction', '6592810717', 'mnansoni@pcworld.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (20, 'Courtenay Slater', '28 Monica Court', '4552922209', 'cslaterj@house.gov');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (21, 'Phillis Rowlstone', '3 Drewry Alley', '7375810403', 'prowlstonek@gizmodo.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (22, 'Alane Messitt', '58111 Knutson Plaza', '5449042328', 'amessittl@livejournal.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (23, 'Corissa Ondrousek', '960 Farragut Drive', '9068408460', 'condrousekm@yahoo.co.jp');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (24, 'Garrard Rudall', '384 Crest Line Pass', '2186798307', 'grudalln@ebay.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (25, 'Sella Robillart', '29 Lake View Pass', '2882764688', 'srobillarto@addtoany.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (26, 'Debby Huws', '34 Basil Court', '2956783703', 'dhuwsp@slideshare.net');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (27, 'Wendi Stitwell', '8175 Loeprich Lane', '7895468517', 'wstitwellq@mtv.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (28, 'Manuel Crewdson', '91 Ludington Trail', '4715709544', 'mcrewdsonr@sbwire.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (29, 'Lorianne Aitken', '91 Quincy Drive', '8348653895', 'laitkens@bbc.co.uk');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (30, 'Rusty MacGuigan', '923 Eagle Crest Drive', '7361449120', 'rmacguigant@jugem.jp');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (31, 'Alvis Mort', '2 Ruskin Trail', '6707812591', 'amortu@vinaora.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (32, 'Gilli Holton', '14712 Haas Point', '6665475575', 'gholtonv@uiuc.edu');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (33, 'Corabel Pallister', '2 Del Mar Street', '7741562194', 'cpallisterw@epa.gov');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (34, 'Janella Matusson', '54 Cascade Crossing', '4043736774', 'jmatussonx@amazonaws.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (35, 'Hilario Klimentyev', '640 Vermont Street', '1185676418', 'hklimentyevy@goo.gl');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (36, 'Pamela Tidswell', '20365 Heath Circle', '4058630245', 'ptidswellz@miitbeian.gov.cn');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (37, 'Silvester Dessaur', '79871 Monument Park', '4787074422', 'sdessaur10@apache.org');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (38, 'Jobi Luckham', '6 Riverside Junction', '4742665639', 'jluckham11@acquirethisname.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (39, 'Jarrad Barlee', '219 Walton Center', '2656698115', 'jbarlee12@alexa.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (40, 'Patrizio Aizikowitz', '349 Maple Terrace', '3552126793', 'paizikowitz13@typepad.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (41, 'Wilma Althorp', '8 Cottonwood Place', '6811865424', 'walthorp14@nbcnews.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (42, 'Valentino Darell', '7 Ridge Oak Pass', '8326455281', 'vdarell15@ovh.net');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (43, 'Hortense Peirone', '0177 Emmet Junction', '4626859004', 'hpeirone16@google.co.jp');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (44, 'Tann Klinck', '06 Onsgard Park', '3173558861', 'tklinck17@elpais.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (45, 'Carol Beernt', '459 Paget Plaza', '4543614777', 'cbeernt18@examiner.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (46, 'Blancha Cruft', '96 Marcy Avenue', '2091210672', 'bcruft19@java.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (47, 'Winny Provest', '2959 Bashford Point', '4671062743', 'wprovest1a@mlb.com');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (48, 'Devon Harmond', '6826 Ridge Oak Hill', '3844579693', 'dharmond1b@telegraph.co.uk');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (49, 'Jervis Chrismas', '230 Pond Court', '8433715583', 'jchrismas1c@cmu.edu');
+insert into contactos (contacto_id, nombre, direccion, telefono, email) values (50, 'Moishe Daal', '877 Harbort Place', '8235181249', 'mdaal1d@simplemachines.org');
