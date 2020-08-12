1ae6cefb1d4bfc999cc26dd3c3f0942e668e8878
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-12 11:57:39 -0400

    format

--- src/main/java/io/github/rhacs/contactos/ContactosApplication.java
+++ src/main/java/io/github/rhacs/contactos/ContactosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContactosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContactosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContactosApplication.class, args);
+    }
 
 }
